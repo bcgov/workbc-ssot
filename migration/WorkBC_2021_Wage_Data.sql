1b11fd18d8f746cc5b6a7a6321943b55fee64f08
infojunkie <karim.ratib@gmail.com>
2022-06-26 22:32:38 -0700

    Add settings to avoid trimming whitespace in SQL files

--- migration/WorkBC_2021_Wage_Data.sql
+++ migration/WorkBC_2021_Wage_Data.sql
@@ -7,6 +7,8 @@ alter table t_0 rename "ESDC Wage Rate Median
 alter table t_0 rename "NOC" to noc;
 alter table t_0 rename "Occupation Title" to occupation_title;
 alter table t_0 rename "Source Information" to source_information;
+-- WARNING! There's a space following "Calculated Median Annual Salary ".
+-- Make sure your text editor does not trim spaces from line endings.
 alter table t_0 rename "Calculated Median Annual Salary 
 2021" to calculated_median_annual_salary_2021;
 create unique index noc_index on t_0(noc);
