5639719fa29c9e8bcf04d96dfc96825d41e5c577
infojunkie <karim.ratib@gmail.com>
2022-06-28 15:39:15 -0700

    Migrate skills and competencies

--- migration/migrate.sh
+++ migration/migrate.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-# Usage: migrate.sh /path/to/data-file [postgresql-connection-string]
+# Usage: migrate.sh /path/to/data-file [pgsql://user:pass@host/db]
 #
 # Operation:
 # - Reads a data file (xlsx, csv, etc.) into a SQLite database
-# - Runs a local query file with same filename as data file against the SQLite database
-# - Optionally load the SQLite tables into the specified PostgreSQL database
+# - Runs a local query file with same filename as data file against the SQLite database (with spaces converted to _underscores_)
+# - Optionally load the SQLite tables into the specified PostgreSQL database if connection string is supplied
 #
 # Dependencies:
 # - SQLite (https://www.sqlite.org/)
@@ -14,9 +14,10 @@
 
 set -e
 dsq -C "$1" "select * from {}" > /dev/null
-database=$(dsq -D "$1" "select * from {}")
-filename=$(basename -- "$1")
-cp "$database" "${filename%.*}.db"
+sqlite=$(dsq -D "$1" "select * from {}")
+basename=$(basename -- "$1")
+filename=${basename// /_}
+cp "$sqlite" "${filename%.*}.db"
 sqlite3 "${filename%.*}.db" < "${filename%.*}.sql"
 if [ -n "$2" ]; then
     pgloader "${filename%.*}.db" "$2"
