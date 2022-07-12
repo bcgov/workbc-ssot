86f6fa0bf965985840366b0603beaa6aa7f0bd22
jinchuantang <79226696+jinchuantang@users.noreply.github.com>
2022-07-12 14:47:38 -0700

    Add files via upload

---
+++ terraform/dev/alb.tf
@@ -0,0 +1,68 @@
+# alb.tf
+
+# Internal Load Balancer
+resource "aws_alb" "main" {
+  name = ""
+  internal           = true
+  load_balancer_type = "application"
+  security_groups    = [data.aws_security_group.web.id]
+  subnets            = module.network.aws_subnet_ids.web.ids 
+
+  enable_deletion_protection = yes
+
+  tags = var.common_tags
+}
+
+# Redirect all traffic from the ALB to the target group
+resource "aws_alb_listener" "front_end" {
+  load_balancer_arn = aws_alb.main.arn
+  port              = "3000"
+  protocol          = "HTTP"
+  
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.app.arn
+  }
+}
+
+resource "aws_alb_target_group" "app" {
+  name                 = "workbc-ssot-target-group-${substr(uuid(), 0, 3)}"
+  port                 = 80
+  protocol             = "HTTPS"
+  vpc_id               = module.network.aws_vpc.id
+  target_type          = "ip"
+  deregistration_delay = 30
+
+  health_check {
+    healthy_threshold   = "5"
+    interval            = "30"
+    protocol            = "HTTPS"
+    matcher             = "200"
+    timeout             = "5"
+    path                = var.health_check_path
+    unhealthy_threshold = "2"
+  }
+    
+  lifecycle {
+    create_before_destroy = true
+    ignore_changes        = [name]
+  }
+
+  tags = var.common_tags
+}
+
+resource "aws_lb_listener_rule" "host_based_weighted_routing" {
+  listener_arn = data.aws_alb_listener.front_end.arn
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.app.arn
+  }
+
+#  condition {
+#    host_header {
+#      values = [for sn in var.service_names : "${sn}.*"]
+#    }
+#  }
+    
+}
