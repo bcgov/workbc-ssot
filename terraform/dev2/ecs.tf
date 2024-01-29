0b6c1f8a3d224faface13f89f1637d7f5e183b73
jinchuantang <jinchuant@gmail.com>
2024-01-29 15:02:02 -0800

    second DEV

---
+++ terraform/dev2/ecs.tf
@@ -0,0 +1,140 @@
+# ecs.tf
+
+resource "aws_ecs_cluster" "main" {
+  name               = "workbc-ssot-cluster2"
+  capacity_providers = ["FARGATE_SPOT"]
+
+  default_capacity_provider_strategy {
+    capacity_provider = "FARGATE_SPOT"
+    weight            = 100
+  }
+
+  tags = var.common_tags
+}
+
+resource "aws_ecs_task_definition" "app" {
+#  count                    = local.create_ecs_service
+  count                    = 1
+  family                   = "workbc-ssot-task2"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  task_role_arn            = aws_iam_role.workbc_ssot_container_role.arn
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = var.fargate_cpu
+  memory                   = var.fargate_memory
+  tags                     = var.common_tags
+
+
+  container_definitions = jsonencode([
+	{
+		essential   = true
+		name        = "postgrest"
+		image       = "${var.app_repo}/postgrest:1.0"
+		networkMode = "awsvpc"
+		
+		logConfiguration = {
+			logDriver = "awslogs"
+			options = {
+				awslogs-create-group  = "true"
+				awslogs-group         = "/ecs/${var.app_name}"
+				awslogs-region        = var.aws_region
+				awslogs-stream-prefix = "ecs"
+			}
+		}		
+
+		portMappings = [
+			{
+				hostPort = 3000
+				protocol = "tcp"
+				containerPort = 3000
+			}
+		]
+		
+		environment = [
+			{
+				name = "PGRST_DB_URI",
+				value = "${local.conn_str}"
+			},
+			{
+				name = "PGRST_DB_SCHEMA",
+				value = "public"
+			},
+			{
+				name = "PGRST_DB_ANON_ROLE",
+				value = "ssot_readonly"
+			},
+			{
+				name = "PGRST_OPENAPI_SERVER_PROXY_URI",
+				value = "http://localhost:3000"
+			}
+		]
+
+
+
+	},
+	{
+		essential   = false
+		name        = "swagger"
+		image       = "${var.app_repo}/swagger:1.0"
+		networkMode = "awsvpc"
+
+		portMappings = [
+			{
+				hostPort = 8080
+				protocol = "tcp"
+				containerPort = 8080
+			}
+		]
+		environment = [
+			{
+				name = "API_URL",
+				value = "http://localhost:3000/"
+			}
+		]
+
+		dependsOn = [
+			{
+				containerName = "postgrest"
+				condition = "START"
+			}
+		]
+	}
+  ])
+}
+
+resource "aws_ecs_service" "main" {
+#  count                             = local.create_ecs_service
+  count                             = 1
+  name                              = "workbc-ssot-service2"
+  cluster                           = aws_ecs_cluster.main.id
+  task_definition                   = aws_ecs_task_definition.app[count.index].arn
+  desired_count                     = var.app_count
+  enable_ecs_managed_tags           = true
+  propagate_tags                    = "TASK_DEFINITION"
+#  health_check_grace_period_seconds = 60
+  wait_for_steady_state             = false
+  enable_execute_command            = true
+
+
+  capacity_provider_strategy {
+    capacity_provider = "FARGATE_SPOT"
+    weight            = 100
+  }
+
+
+  network_configuration {
+    security_groups  = [data.aws_security_group.app.id]
+    subnets          = module.network.aws_subnet_ids.app.ids
+    assign_public_ip = false
+  }
+
+  load_balancer {
+    target_group_arn = aws_alb_target_group.app.id
+    container_name   = "postgrest"
+    container_port   = var.app_port
+  }
+
+  depends_on = [aws_alb_listener.front_end, aws_iam_role_policy_attachment.ecs_task_execution_role]
+
+  tags = var.common_tags
+}
---
+++ terraform/dev2/variables.tf
@@ -0,0 +1,87 @@
+# variables.tf
+
+variable "target_env" {
+  description = "AWS workload account env (e.g. dev, test, prod, sandbox, unclass)"
+  default = "dev"
+}
+
+variable "aws_region" {
+  description = "The AWS region things are created in"
+  default     = "ca-central-1"
+}
+
+variable "ecs_task_execution_role_name" {
+  description = "ECS task execution role name"
+  default     = "workbcSsotEcsTaskExecutionRole"
+}
+
+variable "app_name" {
+  description = "Name of the application"
+  type        = string
+  default     = "workbc-ssot2"
+}
+
+variable "app_image" {
+  description = "Docker image to run in the ECS cluster. _Note_: there is a blank default value, which will cause service and task resource creation to be supressed unless an image is specified."
+  type        = string
+  default     = ""
+}
+
+variable "app_repo" {
+  description = "ECR docker image repo"
+  type        = string
+  default     = ""
+}
+
+variable "app_port" {
+  description = "Port exposed by the docker image to redirect traffic to"
+  default     = 3000
+}
+
+variable "app_count" {
+  description = "Number of docker containers to run"
+  default     = 1
+}
+
+variable "health_check_path" {
+  default = "/"
+}
+
+variable "fargate_cpu" {
+  description = "Fargate instance CPU units to provision (1 vCPU = 1024 CPU units)"
+  default     = 2048
+}
+
+variable "fargate_memory" {
+  description = "Fargate instance memory to provision (in MiB)"
+  default     = 4096
+}
+
+variable "common_tags" {
+  description = "Common tags for created resources"
+  default = {
+    Application = "SSoT DEV2"
+  }
+}
+
+variable "service_names" {
+  description = "List of service names to use as subdomains"
+  default     = ["workbc"]
+  type        = list(string)
+}
+
+variable "alb_name" {
+  description = "Name of the internal alb"
+  default     = "default"
+  type        = string
+}
+
+variable "cloudfront" {
+  description = "enable or disable the cloudfront distrabution creation"
+  type        = bool
+}
+
+variable "cloudfront_origin_domain" {
+  description = "domain name of the app"
+  type        = string
+}
