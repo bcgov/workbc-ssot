3e3a94e61008c2dba368e60e7f20c28c5b7694b9
jinchuantang <79226696+jinchuantang@users.noreply.github.com>
2024-02-20 10:50:45 -0800

    Update alb.tf

--- terraform/dev2/alb.tf
+++ terraform/dev2/alb.tf
@@ -19,7 +19,7 @@ resource "aws_lb_listener_rule" "host_based_weighted_routing" {
 
   condition {
     host_header {
-      values = [for sn in var.service_names : "${sn}.*"]
+      values = [for sn in var.service_names2 : "${sn}.*"]
     }
   }
     
