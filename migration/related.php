486aef4c49d12367d6c9be33cef7b345dca81971
infojunkie <karim.ratib@gmail.com>
2025-07-02 14:59:36 -0700

    [WBCAMS-603] Prevent duplicates

--- migration/related.php
+++ migration/related.php
@@ -23,10 +23,12 @@ $elements = fopen_or_die($filename); fgetcsv($elements);
 $row = 2;
 $ngrams = [];
 $deferred = [];
+$seen = [];
 while (FALSE !== ($element = fgetcsv($elements))) {
   $noc = in_array($element[COLUMN_STRUCTURE_NOC], [
     '00011', '00012', '00013', '00014', '00015'
   ]) ? '00018' : $element[COLUMN_STRUCTURE_NOC];
+  $seen[$noc] ??= [];
 
   // Insert NOC in 4-gram structure.
   $ngram = substr($noc, 0, 4);
@@ -44,12 +46,17 @@ while (FALSE !== ($element = fgetcsv($elements))) {
       '00011', '00012', '00013', '00014', '00015'
     ]) ? '00018' : $matches[1];
 
+    // If we've already seen this related NOC, ignore it.
+    if (in_array($related_noc, $seen[$noc])) {
+      // Do nothing.
+    }
     // If it's a 5-digit NOC, output it immediately.
-    if (strlen($related_noc) === 5) {
+    else if (strlen($related_noc) === 5) {
       fputcsv(STDOUT, [
         $noc,
         $related_noc,
       ]);
+      $seen[$noc][] = $related_noc;
     }
     // If it's a 4-digit NOC, defer it until we've built the full 4-grams structure.
     else if (strlen($related_noc) === 4) {
@@ -66,10 +73,16 @@ while (FALSE !== ($element = fgetcsv($elements))) {
 foreach ($deferred as $noc => $related_ngrams) {
   foreach (array_unique($related_ngrams) as $ngram) {
     foreach (array_unique($ngrams[$ngram]) as $related_noc) {
-      fputcsv(STDOUT, [
-        $noc,
-        $related_noc,
-      ]);
+      if (in_array($related_noc, $seen[$noc])) {
+        // Do nothing.
+      }
+      else {
+        fputcsv(STDOUT, [
+          $noc,
+          $related_noc,
+        ]);
+        $seen[$noc][] = $related_noc;
+      }
     }
   }
 }
@@ -79,8 +92,8 @@ function fopen_or_die($filename) {
   $path = $dirname . $filename;
   $fh = fopen($path, 'r');
   if (!$fh) {
-      fwrite(STDERR, "File $path not found. Aborting.\n");
-      exit(1);
+    fwrite(STDERR, "File $path not found. Aborting.\n");
+    exit(1);
   }
   return $fh;
 }
